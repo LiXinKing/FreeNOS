93dcd5010460b30ec7cfe672b3bfe97c23a5eb71
root <root@debian.king>
2016-10-20 22:30:34 +0800

    ADD NEW COMMAND : PWD

---
+++ bin/pwd/Main.cpp
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2009 Niek Linnenbank
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <TerminalCodes.h>
+#include <Runtime.h>
+
+int main(int argc, char **argv)
+{
+    char path[255];
+
+    getcwd(path, sizeof(path));
+
+    printf("%s", YELLOW);
+    printf("%s", path);
+    printf("\r\n");
+    
+    /* Success. */
+    return EXIT_SUCCESS;
+}
